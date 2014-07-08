61cc8ba1f4e573a82b67c208743931599fd55273
vitorlima <vitor_lp@yahoo.com>
2014-07-08 09:36:02 -0300

    First commit.

---
+++ filtravel.js
@@ -0,0 +1,15 @@
+/*********************************************************
+*	A simple way to filter page's elements
+*
+*	With this plugin, you can search and filter elements
+* of a page, showing or hiding him, by typing any text.
+*
+**********************************************************/
+
+(function($){
+$.fn.filtravel = function(options){
+    var settings = {};
+
+
+};
+}(jQuery));
